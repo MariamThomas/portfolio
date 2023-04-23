c598f840310b893b0dbda2983966a0996436a943
MariamThomas <mthms2002@gmail.com>
2023-04-23 17:06:56 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "React Project",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "React Project",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "React Project",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "React Project",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+    "Working with Elio was a great experience! They were able to understand our needs and come up with innovative solutions that exceeded our expectations. Their technical skills and attention to detail were exceptional, and they were always responsive and professional throughout the project. We look forward to working with them again in the future.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+    "We hired Elio to develop a custom software application for our business, and we were blown away by the results. They were able to deliver the project on time and on budget, and the quality of their work was outstanding. They were also great communicators and always kept us informed throughout the development process. We highly recommend Elio to anyone looking for a skilled and reliable computer engineer.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
